a0fcd45477b88c884a40106faf3ebf103ce6e417
DanielMorton <dacmorton@gmail.com>
2019-07-04 19:13:59 -0400

    Return Array Vectors as sums

--- src/collaborative/mat.rs
+++ src/collaborative/mat.rs
@@ -2,32 +2,34 @@ use sprs::{CsMat, CsVec};
 use num_traits::identities::One;
 use num_traits::Num;
 use std::iter::Sum;
+use ndarray::Array1;
 
 pub trait CsMatExt<N: Num + Copy + Default + Sum> {
-    fn col_sum(&self) -> CsVec<N>;
-    fn row_sum(&self) -> CsVec<N>;
+    fn col_sum(&self) -> Array1<N>;
+    fn row_sum(&self) -> Array1<N>;
 
    // fn col_avg(&self) -> CsVec<N>;
    // fn row_avg(&self) -> CsVec<N>;
 
-    fn one_vec(size: usize) -> CsVec<N> {
+    fn one_vec(size: usize) -> Array1<N> {
         let mut ind_vec: Vec<usize> = Vec::with_capacity(size);
-        let one_vec = vec![One::one(); size];
-        for i in 0..size {
-            ind_vec.push(i);
-        }
-        return CsVec::new(size, ind_vec, one_vec);
+        //let one_vec =
+        Array1::from_vec(vec![One::one(); size])
+        //for i in 0..size {
+       //     ind_vec.push(i);
+       // }
+       // return CsVec::new(size, ind_vec, one_vec);
     }
 }
 
 impl<N> CsMatExt<N> for CsMat<N> where N: Num + Copy + Default + Sum {
-    fn col_sum(&self) -> CsVec<N> {
+    fn col_sum(&self) -> Array1<N> {
         let cols = self.cols();
         let col_vec = Self::one_vec(cols);
         self * &col_vec
     }
 
-    fn row_sum(&self) -> CsVec<N> {
+    fn row_sum(&self) -> Array1<N> {
         let rows = self.cols();
         let row_vec = Self::one_vec(rows);
         &self.transpose_view() * &row_vec
