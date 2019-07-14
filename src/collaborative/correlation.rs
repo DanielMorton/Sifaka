b5d0cd5fcf25e9224986e1a67379e3c1542d66f1
DanielMorton <dacmorton@gmail.com>
2019-07-14 09:39:42 -0400

    turns out it was necessary

--- src/collaborative/correlation.rs
+++ src/collaborative/correlation.rs
@@ -19,7 +19,7 @@ pub fn correlation<N, I>(user_item: &CsMatI<N, I>, sim: SimType) -> CsMatI<N, I>
     where I: SpIndex + TryFrom<usize>,
           N: Num + Copy + Default + Sum + Float {
     match sim {
-        UserUser=> &user_item.view() * &user_item.transpose_view(),
-        ItemItem => &user_item.transpose_view() * &user_item.view()
+        SimType::UserUser=> &user_item.view() * &user_item.transpose_view(),
+        SimType::ItemItem => &user_item.transpose_view() * &user_item.view()
     }
 }
