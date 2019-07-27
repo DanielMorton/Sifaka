6e50fdc71071d7f4c78d7e8baa8452fbffdff17b
DanielMorton <dacmorton@gmail.com>
2019-07-27 14:44:37 -0400

    Combine correlation functions

--- src/collaborative/correlation.rs
+++ src/collaborative/correlation.rs
@@ -9,16 +9,39 @@ use crate::collaborative::mat::Value;
 use super::{Correlation, RecommenderType};
 use super::mat::CsMatFloat;
 
+fn correlation<N, I>(
+    user_item: &CsMatI<N, I>,
+    corr: &Correlation,
+    target_user: Option<&CsMatI<N, I>>,
+) -> CsMatI<N, I>
+where
+    I: SpIndex,
+    N: Value + Default + Float,
+{
+    if let Some(tu) = target_user {
+        let (user_norm, target_norm) = match corr {
+            Correlation::Cosine => (user_item.col_normalize(), tu.col_normalize()),
+            Correlation::Pearson => (
+                user_item.col_center().col_normalize(),
+                tu.col_center().col_normalize(),
+            ),
+        };
+        &user_norm.view() * &target_norm.transpose_view()
+    } else {
+        let item_norm = match corr {
+            Correlation::Cosine => user_item.row_normalize(),
+            Correlation::Pearson => user_item.row_center().row_normalize(),
+        };
+        &item_norm.transpose_view() * &item_norm.view()
+    }
+}
+
 pub fn item_correlation<N, I>(user_item: &CsMatI<N, I>, corr: &Correlation) -> CsMatI<N, I>
 where
     I: SpIndex,
     N: Value + Default + Float,
 {
-    let item_norm = match corr {
-        Correlation::Cosine => user_item.row_normalize(),
-        Correlation::Pearson => user_item.row_center().row_normalize(),
-    };
-    &item_norm.transpose_view() * &item_norm.view()
+    correlation(user_item, corr, None)
 }
 
 pub fn user_correlation<N, I>(
@@ -30,7 +53,5 @@ where
     I: SpIndex,
     N: Value + Default + Float,
 {
-    let user_norm = user_item.col_normalize();
-    let target_norm = target_user.col_normalize();
-    &user_norm.view() * &target_norm.transpose_view()
+    correlation(user_item, corr, Some(target_user))
 }
